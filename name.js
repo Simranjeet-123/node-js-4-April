c969f0b4c6ba9412466e779a223a8b6af5e61d6b
Simranjeet Kaur <744302@student.tor.loc>
2019-04-04 11:03:35 -0400

    sum of numbers using while

--- name.js
+++ name.js
@@ -1,10 +1,10 @@
-var user={
-    first_name:"css",
-    last_name:"html",
-    age:4,/*from www.java2s.com*/
-    website:"java2s.com"
-
-};
-for(key in user){
-    console.log(key);
+let a=1;
+let sum=0;
+while(a<=11)
+{
+    
+    sum+=a;
+    a++;
 }
+console.log(sum);
+
